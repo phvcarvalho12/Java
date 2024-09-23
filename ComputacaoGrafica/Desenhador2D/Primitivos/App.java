c5b0bf646d8ec0aad4855f6e5a5e01e551256d46
PedroCarvalho <pedro2vianna@gmail.com>
2024-09-23 19:36:37 -0300

    Revert "atualização"
    This reverts commit 14c1da0365984ab44cf63682db74d6ef0158c03f.

--- ComputacaoGrafica/Desenhador2D/Primitivos/App.java
+++
@@ -1,9 +0,0 @@
-/**
- * Aplicacao para testar primitivos graficos.
- */
-public class App {
-    public static void main(String args[]) {
-    	// Cria e define dimensao da janela (em pixels)
-        new Gui(700, 600); 
-    }
-}
--- ComputacaoGrafica/Desenhador2D/Primitivos/TipoPrimitivo.java
+++
@@ -1,9 +0,0 @@
-/**
- * Enumeration class TiposPrimitivos - write a description of the enum class here
- * 
- * @author (your name here)
- * @version (version number or date here)
- */
-public enum TipoPrimitivo {
-    PONTO, RETA, CIRCULO, NENHUM
-}
